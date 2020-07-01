d5a28a5bb1dafca9c5802b8cad238999a353933f
Eugene Phua <eugene_phua@tech.gov.sg>
2020-07-01 17:58:17 +0800

    Working version of carpark endpoint.

--- main.go
+++ main.go
@@ -8,7 +8,24 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
+
+// Carparks struct which contains an array of carparks
+type Carparks struct {
+	Carparks []Carpark `json:"value"`
+}
+
+// Carpark struct which contains the details
+type Carpark struct {
+	CarParkID     string `json:"CarParkID"`
+	Area          string `json:"Area"`
+	Development   string `json:"Development"`
+	Location      string `json:"Location"`
+	AvailableLots int    `json:"AvailableLots"`
+	LotType       string `json:"LotType"`
+	Agency        string `json:"Agency"`
+}
 
 func main() {
 	router := gin.Default()
@@ -30,45 +47,82 @@ func main() {
 		//value, dataType, offset, err := jsonparser.Get(body, "mid")
 		//fmt.Printf("mid: %s; type: %T; offset: %d; err: %s", value, dataType, offset, err)
 
-		getCurrentTaxiLocations()
-		c.JSON(http.StatusOK, "string(resp)")
+		result := getCurrentTaxiLocations()
+		c.JSON(http.StatusOK, result)
 	})
 
 	router.Run(":8080")
 }
 
-func getCurrentTaxiLocations() {
+func getCurrentTaxiLocations() Carparks {
 
-	url := "https://api.data.gov.sg/v1/transport/carpark-availability"
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
+	url := viperEnvVariable("Carpark")
+	key := viperEnvVariable("AccountKey")
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("AccountKey", key)
+
+	res, err := client.Do(req)
 	body, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Decoding arbitrary data: https://blog.golang.org/json
-	var f interface{}
-	err = json.Unmarshal(body, &f)
+	fmt.Println(len(body))
 
-	// iterate through the map with a range statement 
-	m := f.(map[string]interface{})
-	for k, v := range m {
-		switch vv := v.(type) {
-		case string:
-			fmt.Println(k, "is string", vv)
-		case float64:
-			fmt.Println(k, "is float64", vv)
-		case []interface{}:
-			fmt.Println(k, "is an array:")
-			for i, u := range vv {
-				fmt.Println(i, u)
-			}
-		default:
-			fmt.Println(k, "is of a type I don't know how to handle")
-		}
+	var carparks Carparks
+	json.Unmarshal(body, &carparks)
+
+	//fmt.Println(len(carparks.Carparks))
+
+	return carparks
+
+	// // Decoding arbitrary data: https://blog.golang.org/json
+	// var f interface{}
+	// err = json.Unmarshal(body, &f)
+
+	// // iterate through the map with a range statement
+	// m := f.(map[string]interface{})
+	// for k, v := range m {
+	// 	switch vv := v.(type) {
+	// 	case string:
+	// 		fmt.Println(k, "is string", vv)
+	// 	case float64:
+	// 		fmt.Println(k, "is float64", vv)
+	// 	case []interface{}:
+	// 		fmt.Println(k, "is an array:")
+	// 		for i, u := range vv {
+	// 			fmt.Println(i, u)
+	// 		}
+	// 	default:
+	// 		fmt.Println(k, "is of a type I don't know how to handle")
+	// 	}
+	// }
+}
+
+// return the value of the key
+func viperEnvVariable(key string) string {
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
 	}
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(key).(string)
+
+	// If the type is a string then ok will be true
+	// ok will make sure the program not break
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value
 }
